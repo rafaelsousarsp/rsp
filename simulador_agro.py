05d409ecfd3a09361d4df7588ba788558b7b12f7
RSP <rafaelsousarsp@gmail.com>
2025-12-16 13:15:49 -0400

    Change step value for area slider to 0.1

--- simulador_agro.py
+++ simulador_agro.py
@@ -16,7 +16,7 @@ st.sidebar.header("🎯 Simulador")
 
 # Entradas do usuário
 produtividade = st.sidebar.slider ("Produtividade (sacas/ha):", 0.0, 200.0, 0.0, step =0.01 )
-area = st.sidebar.slider ("Área Plantada (ha):", 0.0, 20000.0, 0.0, step=0.01)
+area = st.sidebar.slider ("Área Plantada (ha):", 0.0, 20000.0, 0.0, step=0.1)
 preco = st.sidebar.slider("Preço Médio de Venda (R$):", 0.0, 200.0, 0.0, step=0.01)
 custo_producao = st.sidebar.slider("Custo de Produção Total (R$):", 0.0, 100000000.0, 0.0, step=100.0)
 desp_nao_op = st.sidebar.slider("Despesas Não Operacionais (R$):", 0.0, 100000000.0, 0.0, step=100.0)
